3eae9aa0292c8586174586a5657d1f852fab6278
deltabravo777 <97050711+deltabravo777@users.noreply.github.com>
2022-01-03 11:09:06 -0800

    Add files via upload

---
+++ ASPNET_Project_Ten/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace ASPNET_Project_Eleven
+{
+    // To Do List:
+    //
+    // [~] Find a way to center the navbar without breaking it
+    // [~] carousel-inner has a fixed height of 500px. this makes it look weird on phone screens
+    // [ ] categories for articles
+    // [ ] hyperlink in body
+    // [ ] images as background
+    // [ ] connection string built in the Startup class
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
+
