b93037c72b820d46f432dd4778af5a017e59bb84
deltabravo777 <97050711+deltabravo777@users.noreply.github.com>
2022-01-11 20:15:01 -0800

    Update Playlist.cs

--- ASPNET_Project_Ten/Models/Playlist.cs
+++ ASPNET_Project_Ten/Models/Playlist.cs
@@ -6,8 +6,8 @@ namespace ASPNET_Project_Eleven.Models
 {
     public class Playlist
     {
-        [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        // [Key]
+        // [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         [NotMapped]
         public string EncryptedId { get; set; }
